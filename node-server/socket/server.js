40c196250b31b325e45d779083af3973050b925c
chenu <chenulfan@gmail.com>
2024-03-03 14:37:15 +0200

    Added on new message
    Signed-off-by: chenu <chenulfan@gmail.com>

--- node-server/socket/server.js
+++ node-server/socket/server.js
@@ -6,42 +6,84 @@ import redis from './redis.js';
 // App setup
 const PORT = 5000;
 const app = express();
+const JORDI = 'jordi'
+const USER = 'user'
 
 const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
+    console.log(`Listening on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
 
 // Static files
 app.use(cors());
 
 // Socket setup
-const io = new SocketServer(server,{
+const io = new SocketServer(server, {
     cors: {
-      origin: '*',
+        origin: '*',
     }
-  });
+});
 
 io.on("connection", (socket) => {
     console.log("new socket connection: ", socket.id);
-    socket.on('createRoom', ({name, org}) => {
-        console.log(name, org)
-        // socket.join(roomName);
-        // console.log(`User joined room: ${roomName}`);
-      });
-    // redis.set()
+
+    socket.on('createChat', async ({ userId, orgId }) => {
+        const chatId = `${userId}-${orgId}`;
+
+        socket.join(chatId);
+
+        let chatObject = await redis.get(chatId)
+
+        if (chatObject == null) {
+            chatObject = {
+                messages: [],
+            }
+
+            await addMessage(chatObject, chatId, JORDI, `welcome ${chatId}!`)
+        }
+
+        socket.emit('createChat', chatObject);
+    });
+
+    socket.on('newMessage', async ({ userId, orgId, message }) => {
+        const chatId = `${userId}-${orgId}`;
+        const chatObjectStringify = await redis.get(chatId);
+        const chatObject = JSON.parse(chatObjectStringify);
+
+        await addMessage(chatObject, chatId, USER, message)
+        socket.emit('newMessage', chatObject);
+
+        // Run Backend Service
+
+
+    })
 });
 
 io.on("disconnect", (socket) => {
     console.log("socket disconnect");
-  });
+});
 
 
 app.get('/hello', (req, res) => {
     res.send('hello')
 })
 
-app.get('/redis/:userId', (req, res) => {
-    // const userId = req.params.userId;
-    // redis.get(userId)
+app.get('/redis/:roomId', async (req, res) => {
+    const roomId = req.params.roomId;
+    const roomObject = await redis.get(roomId)
+    res.json(roomObject)
+
 })
+
+app.get('/redis/init/:roomId', async (req, res) => {
+    const roomId = req.params.roomId;
+    await redis.set(roomId, null);
+    res.end()
+})
+
+
+const addMessage = (chatObject, chatId, sender, message) => {
+    chatObject.messages.push({ sender, message })
+
+    return redis.set(chatId, JSON.stringify(chatObject));
+}
